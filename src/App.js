c3c2dff1877ed58e81682994782e0e0bbcc49e60
sandhya2304 <sandhyasharma0423@gmail.com>
2020-10-21 02:10:58 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import Home from './component/pages/Home';
+import About from './component/pages/About';
+import Contact from './component/pages/Contact';
+import Navbar from './component/layout/Navbar';
+import { BrowserRouter as Router, Route , Switch } from 'react-router-dom';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+
+      <div className="App">
+
+        <Navbar />
+
+         <switch>
+            <Route exact path ="/" component={Home} />
+            <Route exact path ="/about" component={About} />
+            <Route exact path ="/contact" component={Contact} />
+         </switch>
+
+       
+      </div>
+
+    </Router>
   );
 }
 
---
+++ src/component/pages/About.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+const About  = () =>{
+    return (
+        <div className="container">
+            <div className="py-4">
+                <h1> About Page </h1>
+                <p className="lead">
+                     
+                    
+
+
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default About;
